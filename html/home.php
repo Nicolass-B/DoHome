085e357b9f8f81f412bbf41d729d732f881b6770
Antoine <fallenhaswon@gmail.com>
2017-05-03 10:16:37 +0200

    use css

--- html/home.php
+++ html/home.php
@@ -2,35 +2,45 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/homeStyle.css"/>
+    <link rel="stylesheet" href="../css/skeleton.css"/>
+    <link rel="stylesheet" href="../css/normalize.css"/>
     <title>Accueil DoHome enterprise ™</title>
 </head>
-<body>
+<div>
 
-<img src="../img/fond_transparent.png" alt="logo de l'entreprise"/>
+    <img src="../img/fond_transparent.png" alt="logo de l'entreprise"/>
+</div>
+    <div class="container">
+        <div class="row">
+            <div class="one-half column" style="margin-top: 25%">
+                <h1>DoHome enterprise ™</h1>
+                <p> l'approche sociale dans notre pays encourage la diversité
+                    et la sensibilité d'un mouvement cheminant vers plus d'unité.
+                    En effet, c'est en toute prescience que je peux garantir
+                    présentement que la sécurité des personnes confirme
+                    l'inaliénable volonté d'un temps et d'une époque en phase
+                    avec les innovations afin de ne pas être dépassé.
+                    Beaucoup de nos concitoyens n’ont pas encore fait leur
+                    choix et pensent que la tradition attachée à notre mémoire
+                    consubstantielle justifie les subventions publiques d'un
+                    déploiement visionnaire revivifiant. De plus, vous
+                    connaissez certainement la situation qui mène à dire que
+                    la dynamique vertueuse doit correspondre aux directives
+                    indispensables préalablement établies afin de poursuivre
+                    l'effort plus que légitime d'une majorité parlementaire
+                    qui responsabiliserait sur l'urgence écologique.</p>
 
-<h1>DoHome enterprise ™</h1>
 
-<p>l'approche sociale dans notre pays encourage la diversité
-    et la sensibilité d'un mouvement cheminant vers plus d'unité.
-    En effet, c'est en toute prescience que je peux garantir
-    présentement que la sécurité des personnes confirme
-    l'inaliénable volonté d'un temps et d'une époque en phase
-    avec les innovations afin de ne pas être dépassé.
-    Beaucoup de nos concitoyens n’ont pas encore fait leur
-    choix et pensent que la tradition attachée à notre mémoire
-    consubstantielle justifie les subventions publiques d'un
-    déploiement visionnaire revivifiant. De plus, vous
-    connaissez certainement la situation qui mène à dire que
-    la dynamique vertueuse doit correspondre aux directives
-    indispensables préalablement établies afin de poursuivre
-    l'effort plus que légitime d'une majorité parlementaire
-    qui responsabiliserait sur l'urgence écologique.</p>
+            </div>
+        </div>
+    </div>
 
-<a href="signUp.php" target="_blank">inscription</a>
+    <div class="container">
 
-<p>Powered By DoHome Enterprise™ + ♥ from Paris</p>
-<p>Mentions Légales</p>
+        <a href="signUp.php" target="_blank">inscription</a>
+        <p>Powered By DoHome Enterprise™ + ♥ from Paris</p>
+        <p>Mentions Légales</p>
 
-</body>
+    </div>
+    </body>
 </html>
