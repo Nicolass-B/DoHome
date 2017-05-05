6f0b89737076e09ee3eb3ef9557acb5166aaf45c
clementmosser <clement-mosser@live.fr>
2017-05-05 10:23:25 +0200

    Message

---
+++ html/Dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html xmlns:justify xmlns:text-justify="http://www.w3.org/1999/xhtml">
+
+<head>
+    <link rel="stylesheet" href="../html/Dashboard_CSS.css"/>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+</head>
+
+<body>
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="../img/fond_transparent2.png" alt="logo de l'entreprise">
+        </div>
+        <div class="liste">
+            <ul id="menu">
+                <li>Dashboard</li>
+                <li><bord>Mon profil</bord></li>
+                <li><bord2>Ma maison</bord2></li>
+                <li>Boutique</li>
+            </ul>
+        </div>
+    </div>
+</header>
+
+</body>
+</html>
