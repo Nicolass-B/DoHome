30c55454c91bebbebf1b6075eed00413c165f6f3
Nico <nicolas.benmennad@isep.fr>
2017-05-05 11:49:29 +0200

    Ajout page d'accueil et page d'inscription

---
+++ php_html/home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html xmlns:justify xmlns:text-justify="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/skeleton.css"/>
+    <link rel="stylesheet" href="../css/normalize.css"/>
+    <title>Accueil DoHome enterprise ™</title>
+</head>
+<body>
+
+
+<header>
+    <div class="container1">
+        <figure>
+            <img class="logo" src="../img/fond_transparent.png" alt="logo de l'entreprise"/>
+        </figure>
+
+        <p>
+            DoHome <span>enterprise ™</span>
+        </p>
+    </div>
+</header>
+
+
+<section>
+    <div class="textIntro">
+        <p>
+            l'approche sociale dans notre pays encourage la diversité
+            et la sensibilité d'un mouvement cheminant vers plus d'unité.
+            En effet, c'est en toute prescience que je peux garantir
+            présentement que la sécurité des personnes confirme
+            l'inaliénable volonté d'un temps et d'une époque en phase
+            avec les innovations afin de ne pas être dépassé.
+            Beaucoup de nos concitoyens n’ont pas encore fait leur
+            choix et pensent que la tradition attachée à notre mémoire
+            consubstantielle justifie les subventions publiques d'un
+            déploiement visionnaire revivifiant. De plus, vous
+            connaissez certainement la situation qui mène à dire que
+            la dynamique vertueuse doit correspondre aux directives
+            indispensables préalablement établies afin de poursuivre
+            l'effort plus que légitime d'une majorité parlementaire
+            qui responsabiliserait sur l'urgence écologique.
+        </p>
+    </div>
+</section>
+
+<section>
+    <div class="container2">
+     <form method="post" action="connexion.php">
+
+       <input type="text" name="pseudo" id="pseudo" placeholder="Login" autofocus required/>
+       <input type="password" name="pass" id="pass" placeholder="Mot de passe" required/>
+       <input type="submit" value="Se connecter" />
+         <p>
+             <input type="radio" name="memo" id="memo"/>
+             <label for="memo">Se souvenir de moi</label>
+         </p>
+     </form>
+    </div>
+</section>
+
+
+
+<footer>
+    <div class="container3">
+        <div class="powered">Powered By DoHome Enterprise™ + ♥ from Paris</div>
+        <div class="legalMention">Mentions Légales</div>
+    </div>
+</footer>
+
+
+</body>
+</html>
