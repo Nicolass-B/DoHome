ae8a339f2d568da71464566a562dbe024dbcb8b0
clementmosser <clement-mosser@live.fr>
2017-05-09 10:09:45 +0200

    Message

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # DoHome
 Site web DoHome enterprise
+APP 2017 - Informatiquee
 
-## blablabla
+## Répartition des taches
+
+- Guillaume : BDD
+- Clément : Dashboard
+- Antoine : Mes Capteurs / Capteur / Gabarit_loggedon
+- Sébastien : Backoffice, FAQ, Boutique
+- Ali :
+- Nicolas
+
+## TODO
+
+- things
+- Other things
--- html/Dashboard.php
+++ html/Dashboard.php
@@ -8,115 +8,154 @@
 </head>
 
 <body>
+
 <header>
     <div class="container">
             <img class="logo" src="../img/fond_transparent2.png" alt="logo de l'entreprise">
-        <div class="liste">
-            <ul id="menu">
+        <nav>
+            <ul class="menu">
                 <li>Dashboard</li>
-                <li><bord>Mon profil</bord></li>
-                <li><bord2>Ma maison</bord2></li>
+                <li>Mon profil</li>
+                <li>Ma maison</li>
                 <li>Boutique</li>
             </ul>
-        </div>
+        </nav>
     </div>
 </header>
 
 <section>
-    <section1>
-        <div class="information">
+    <HD>
 
-            <div class="alarme">
-                <p>Alarme</p>
-                <script type="text/javascript">
-                    function onoff(){
-                        currentvalue = document.getElementById('onoff').value;
-                        if(currentvalue == "Off"){
-                            document.getElementById("onoff").value="On";
-                        }
-                        else{
-                            document.getElementById("onoff").value="Off";
-                        }
-                    }
-                </script>
+        <section1>
+            <div class="information">
 
-                <input type="button" value="On" id="onoff" onclick="onoff();">
+                <div class="alarme">
+                    <p>Alarme</p>
+                    <script type="text/javascript">
+                        function onoff(){
+                            currentvalue = document.getElementById('onoff').value;
+                            if(currentvalue == "Off"){
+                                document.getElementById("onoff").value="On";
+                            }
+                            else{
+                                document.getElementById("onoff").value="Off";
+                            }
+                        }
+                    </script>
+
+                    <input type="button" value="On" id="onoff" onclick="onoff();">
+                </div>
+                <div class="mode">
+                    <p>Mode</p>
+                    <form method="post" action="mode.php">
+                        <select name="mode" id="mode">
+                            <option value="nuit">Nuit</option>
+                            <option value="jour">Jour</option>
+                            <option value="vacances">Vacances</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="température">
+                    <p>Température</p>
+                    <form method="post" action="mode.php">
+                        <select name="température" id="température">
+                            <option value="15">15°C</option>
+                            <option value="16">16°C</option>
+                            <option value="17">17°C</option>
+                            <option value="18">18°C</option>
+                            <option value="19">19°C</option>
+                            <option value="20">20°C</option>
+                            <option value="21">21°C</option>
+                            <option value="22">22°C</option>
+                            <option value="23">23°C</option>
+                            <option value="24">24°C</option>
+                            <option value="25">25°C</option>
+                        </select>
+                    </form>
+                </div>
             </div>
-            <div class="mode">
-                <p>Mode</p>
-                <form method="post" action="mode.php">
-                    <select name="mode" id="mode">
-                        <option value="nuit">Nuit</option>
-                        <option value="jour">Jour</option>
-                        <option value="vacances">Vacances</option>
-                    </select>
-                </form>
-            </div>
-            <div class="température">
-                <p>Température</p>
-                <form method="post" action="mode.php">
-                    <select name="température" id="température">
-                        <option value="15">15°C</option>
-                        <option value="16">16°C</option>
-                        <option value="17">17°C</option>
-                        <option value="18">18°C</option>
-                        <option value="19">19°C</option>
-                        <option value="20">20°C</option>
-                        <option value="21">21°C</option>
-                        <option value="22">22°C</option>
-                        <option value="23">23°C</option>
-                        <option value="24">24°C</option>
-                        <option value="25">25°C</option>
-                    </select>
-                </form>
-            </div>
-        </div>
-    </section1>
-    <section2>
-        <div class="information">
-            <div class="volets">
-                <p>Volets</p>
-                <script type="text/javascript">
-                    function Ouverture(){
-                        currentvalue = document.getElementById('Ouverture').value;
-                        if(currentvalue == "Ouvrir"){
-                            document.getElementById("Ouverture").value="Fermer";
-                        }
-                        else{
-                            document.getElementById("Ouverture").value="Ouvrir";
-                        }
-                    }
-                </script>
-                <input type="button" value="Ouvrir" id="Ouverture" onclick="Ouverture();">
+        </section1>
 
+        <section2>
+            <div class="information">
+                <div class="volets">
+                    <p>Volets</p>
+                    <script type="text/javascript">
+                        function Ouverture(){
+                            currentvalue = document.getElementById('Ouverture').value;
+                            if(currentvalue == "Ouvrir"){
+                                document.getElementById("Ouverture").value="Fermer";
+                            }
+                            else{
+                                document.getElementById("Ouverture").value="Ouvrir";
+                            }
+                        }
+                    </script>
+                    <input type="button" value="Ouvrir" id="Ouverture" onclick="Ouverture();">
+
+                </div>
+
+                <div class="lumiere">
+                    <p>Lumières</p>
+                    <script type="text/javascript">
+                        function Allumage(){
+                            currentvalue = document.getElementById('Allumage').value;
+                            if(currentvalue == "Allumer"){
+                                document.getElementById("Allumage").value="Eteindre";
+                            }
+                            else{
+                                document.getElementById("Allumage").value="Allumer";
+                            }
+                        }
+                    </script>
+                    <input type="button" value="Allumer" id="Allumage" onclick="Allumage();">
+                </div>
             </div>
 
-            <div class="lumiere">
-                <p>Lumières</p>
-                <script type="text/javascript">
-                    function Allumage(){
-                        currentvalue = document.getElementById('Allumage').value;
-                        if(currentvalue == "Allumer"){
-                            document.getElementById("Allumage").value="Eteindre";
-                        }
-                        else{
-                            document.getElementById("Allumage").value="Allumer";
-                        }
-                    }
-                </script>
-                <input type="button" value="Allumer" id="Allumage" onclick="Allumage();">
+        </section2>
+
+        <section3>
+            <div class="information">
+                <div class="meteo">
+                    <div id="cont_NzUwNTZ8NXwyfDR8NXxCN0I3Qjd8MXwwMDAwMDB8Y3wx">
+                        <div id="spa_NzUwNTZ8NXwyfDR8NXxCN0I3Qjd8MXwwMDAwMDB8Y3wx">
+                            <a id="a_NzUwNTZ8NXwyfDR8NXxCN0I3Qjd8MXwwMDAwMDB8Y3wx" href="http://www.meteocity.com/france/paris_v75056/" target="_blank" style="color:#333;text-decoration:none;">Météo Paris</a> ©
+                            <a href="http://www.meteocity.com">meteocity.com</a>
+                        </div>
+                        <script type="text/javascript" src="http://widget.meteocity.com/js/NzUwNTZ8NXwyfDR8NXxCN0I3Qjd8MXwwMDAwMDB8Y3wx">
+                        </script>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section3>
 
-    </section2>
+        <section4>
+            <div class="information">
+                <div class="video">
+                    <p>Vidéosurveillance</p>
+                    <video src="../img/Zombieland copie.mp4" controls poster="../img/fond-decran-page2.jpg" width="200">
+                    </video>
+                </div>
+            </div>
+        </section4>
+    </HD>
 
-    <section3>
-
-    </section3>
-
-    http://www.meteofrance.com/previsions-meteo-france/paris-15e-arrondissement/75015#seven-days
+    <BD>
+        <section5>
+            <div class="information">
+                <div class="modification">
+                    <p>Modifier mon Dashboard</p>
+                </div>
+            </div>
+        </section5>
+    </BD>
 
 </section>
+
+<footer>
+    <div class="Powered">Powered By DoHome Enterprise™</div>
+    <div class="Mentions">Mentions Légales</div>
+</footer>
 
 </body>
 </html>
