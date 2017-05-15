a87fbe904fb99c65a3d4268604bd6006ee24baa4
Alionur93 <alionur_93@hotmail.com>
2017-05-15 09:36:57 +0200

    Add files via upload

--- Vue/compteSec.php
+++ Vue/compteSec.php
@@ -2,59 +2,74 @@
 <html>
 <head>
     <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="../css/compteSec.css"/>
+    <link rel="stylesheet" href="../css/profil.css"/>
     <title> Mes comptes secondaires </title>
 </head>
 <body>
 
 <header>
-    <img class="imgpetite" src="img/logo.png" alt="DoHome"/>
-    <nav>
-        <ul>
-            <div class="menu">
-                <li>Dashboard</li>
-                <li>Mon profil</li>
-                <li>Ma maison</li>
-                <li>Boutique</li>
-            </div>
-        </ul>
-    </nav>
+    <section1>
+        <img class="logo" src="../img/fond_transparent3.png" alt="logo de l'entreprise"/>
+        <nav>
+            <ul>
+                <div class="menu">
+                    <li><a href="Dashboard.php">Dashboard</a></li>
+                    <li class="enCours"><a href="monProfil.php">Mon profil</a></li>
+                    <li>Ma maison</li>
+                    <li>Boutique</li>
+                </div>
+            </ul>
+        </nav>
+    </section1>
+    <section1>
+        <div class="recherche">
+            <form action="/search" id="searchthis" method="get">
+                <input id="search" name="q" type="text" placeholder="Rechercher"/>
+                <input id="search-btn" type="submit" value="Rechercher"/>
+            </form>
+        </div>
+
+    </section1>
+    <section1>
+        <img class="mail" src="../img/email.png" alt="logo de l'email"/>
+        <img class="deconexion" src="../img/deconexion.png" alt="logo de la deco"/>
+
+    </section1>
 </header>
 
 <section>
+    <div class="information">
+        <ul>
+            <div class="menusec">
+                <li><a href="monProfil.php">Mon profil</a></li>
+                <li class="enCours"><a href="compteSec.php">Mes comptes secondaires</a></li>
+            </div>
+        </ul>
+        <p>
+            <div class="placement">
+                <div class="imagepp">
+                    <img src="../img/pp%20compte%20sec.png" alt="Photo de profil"/>
+                </div>
 
-    <ul>
-        <div class="menusec">
-            <li><a href="monProfil.php">Mon profil</a></li>
-            <li id="enCours"><a href="compteSec.php">Mes comptes secondaires</a></li>
-        </div>
-    </ul>
+        <p>P + NOM</p>
 
-    <p>
-        <div class="placement">
-            <div class="imagepp">
-                <img src="img/pp compte sec.png" alt="Photo de profil"/>
+        <div class="boutton">
+
+            <div class="bouton1">
+                <input type="button" value="Modifier les accès du compte secondaire" href="lien" id="bouton1"/>
+            </div>
+            <div class="bouton2">
+                <input type="button" value="Supprimer le compte secondaire" href="lien" id="bouton2"/>
             </div>
 
-    <p>P + NOM</p>
-
-    <div class="boutton">
-
-        <div class="bouton1">
-            <input type="button" value="Modifier les accès du compte secondaire" href="lien"/>
         </div>
-        <div class="bouton2">
-            <input type="button" value="Supprimer le compte secondaire" href="lien"/>
-        </div>
-
-    </div>
     </div>
     </p>
 
     <p>
         <div class="placement">
             <div class="imagepp">
-                <img src="img/pp compte sec.png" alt="Photo de profil"/>
+                <img src="../img/pp%20compte%20sec.png" alt="Photo de profil"/>
             </div>
     <p>
         P + NOM
@@ -63,10 +78,10 @@
     <div class="boutton">
 
         <div class="bouton1">
-            <input type="button" value="Modifier les accès du compte secondaire" href="lien"/>
+            <input type="button" value="Modifier les accès du compte secondaire" href="lien" id="bouton1"/>
         </div>
         <div class="bouton2">
-            <input type="button" value="Supprimer le compte secondaire" href="lien"/>
+            <input type="button" value="Supprimer le compte secondaire" href="lien" id="bouton2"/>
         </div>
     </div>
     </div>
@@ -75,25 +90,25 @@
     <p>
         <div class="placement">
             <div class="imagepp">
-                <img src="img/pp compte sec.png" alt="Photo de profil"/>
+                <img src="../img/pp%20compte%20sec.png" alt="Photo de profil"/>
             </div>
     <p>P + NOM</p>
 
     <div class="boutton">
         <div class="bouton1">
-            <input type="button" value="Modifier les accès du compte secondaire" href="lien"/>
+            <input type="button" value="Modifier les accès du compte secondaire" href="lien" id ="bouton1"/>
         </div>
         <div class="bouton2">
-            <input type="button" value="Supprimer le compte secondaire" href="lien"/>
+            <input type="button" value="Supprimer le compte secondaire" href="lien" id="bouton2"/>
         </div>
     </div>
     </div>
     </p>
 
     <div class="bouton3">
-        <input type="button" value="Ajouter un compte secondaire" href="lien"/>
+        <input type="button" value="Ajouter un compte secondaire" href="lien" id="bouton3"/>
     </div>
-
+    </div>
 </section>
 
 </body>
--- Vue/monProfil.php
+++ Vue/monProfil.php
@@ -2,38 +2,58 @@
 <html>
 <head>
     <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="../css/compteSec.css"/>
+    <link rel="stylesheet" href="../css/profil.css"/>
     <title> Mon profil </title>
 </head>
 <body>
 <header>
-    <img class="imgpetite" src="img/logo.png" alt="DoHome"/>
-    <nav>
-        <ul>
-            <div class="menu">
-                <li>Dashboard</li>
-                <li>Mon profil</li>
-                <li>Ma maison</li>
-                <li>Boutique</li>
-            </div>
-        </ul>
-    </nav>
+    <section1>
+        <div class="container">
+            <img class="logo" src="../img/fond_transparent3.png" alt="logo de l'entreprise"/>
+            <nav>
+                <ul>
+                    <div class="menu">
+                        <li><a href="Dashboard.php">Dashboard</a></li>
+                        <li class="enCours"><a href="monProfil.php">Mon profil</a></li>
+                        <li>Ma maison</li>
+                        <li>Boutique</li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </section1>
+    <section1>
+        <div class="recherche">
+            <form action="/search" id="searchthis" method="get">
+                <input id="search" name="q" type="text" placeholder="Rechercher"/>
+                <input id="search-btn" type="submit" value="Rechercher"/>
+            </form>
+        </div>
+    </section1>
+    <section1>
+        <img class="mail" src="../img/email.png" alt="logo de l'email"/>
+        <img class="deconexion" src="../img/deconexion.png" alt="logo de la deco"/>
+
+    </section1>
 </header>
 
 <section>
+    <div class="information">
     <ul>
         <div class="menusec">
-            <li id="enCours"><a href="monProfil.php">Mon profil</a></li>
+            <li class="enCours"><a href="monProfil.php">Mon profil</a></li>
             <li><a href="compteSec.php">Mes comptes secondaires</a></li>
         </div>
     </ul>
+    </div>
     <p>
-        <div class = "formulaire">
+    <div class="formulaire">
+        <div class="information">
         <ul>
             Nom
-            <li><input type="text" name="pseudo" value="Nom" disabled="disabled" /></li>
+            <li><input type="text" name="pseudo" value="Nom" disabled="disabled"/></li>
             Prénom
-            <li><input type="text" name="pseudo" value="Prénom" disabled="disabled" /></li>
+            <li><input type="text" name="pseudo" value="Prénom" disabled="disabled"/></li>
             E-mail
             <li><input type="text" name="pseudo" value="adresse mail"/></li>
             Mot de passe
@@ -41,15 +61,25 @@
             Adresse
             <li><input type="text" name="pseudo" value="adresse mail"/></li>
             Date de naissance
-            <li><input type="date" /></li>
+            <li><input type="date"/></li>
             Numéro de téléphone
             <li><input type="text" name="pseudo" value="numéro"/></li>
 
 
-
         </ul>
         </div>
+    </div>
 
     </p>
 
 </section>
+</body>
+<footer>
+    <p>
+        <span class="centre">Powered By DoHome Enterprise™</span>
+        <span class="droite">Mentions Légales</span>
+    </p>
+
+
+</footer>
+</html>
