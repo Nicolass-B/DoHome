fc0302c0ae91a100c2d7528feecf7032d45afce5
clementmosser <clement-mosser@live.fr>
2017-05-19 10:54:16 +0200

    Changement CSS Home

---
+++ modele/capteur.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Antoine
+ * Date: 05/05/2017
+ * Time: 10:12
+ */
+require_once ("init.php");
+
+// fonction qui cherche les capteurs pour une pièce donnée
+function get_capteurs_piece(PDO $db,$IDpiece){
+
+    $req = $db->prepare('SELECT ID_Capteurs FROM capteurs WHERE ID4=:idpiece');
+    $req->execute(array(
+        'idpiece' => $IDpiece,
+    ));
+    $resultat = $req->fetch();
+
+    return $resultat;
+}
+
+function get_piece_maison(PDO $db,$IDpiece){
+
+    $req = $db->prepare('SELECT ID_Capteurs FROM capteurs WHERE ID4=:idpiece');
+    $req->execute(array(
+        'idpiece' => $IDpiece,
+    ));
+        $resultat = $req->fetch();
+
+    return $resultat;
+}
+
+// fonction qui cherche le mot de passe d'un utilisateur avec un identifiant dans la base de données
+
+function utilisateurs(PDO $db){
+    $reponse = $db->query("SELECT Mail FROM user");
+    return $reponse;
+}
+
+try{
+    //$db = new PDO("mysql:host=$host;dbname=$dbname", "$user");
+    $db->query("SET NAMES UTF8");
+    echo "connexion réussie";
+}
+catch (PDOException $e){
+    die('erreur : '. $e->getMessage());
+}
+
+?>
