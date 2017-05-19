9e4a43506c0cffe83c2a3ad68a8a5ec2bb9178da
ali <alionur_93@hotmail.com>
2017-05-19 11:54:51 +0200

    Merge remote-tracking branch 'origin/Nico-Accueil-inscription-profil' into Nico-Accueil-inscription-profil
    # Conflicts:
    #	modele/capteur.php
    #	modele/utilisateurs.php
    
    Haut de page refaite
    mise en place de haut de page dans toutes les pages
    Amélioration du code et nettoyage de certaines pages css et html pour rendre ça propre
    Optimisation du dashboard admin

--- Vue/DashBoard_BackOffice.php
+++ Vue/DashBoard_BackOffice.php
@@ -11,12 +11,22 @@
 
 
 <body>
+<div class="information">
 <div class="dashAdmin">
-    <a class="LienImage" href="analyseBackOffice.php"><img class="analyse" src="../Vue/img/analyse.png" alt="logo analyse"/></a>
-    <a class="LienImage" href="messagerieBackOffice.php"><img class="messagerie" src="../Vue/img/messagerie.png" alt="logo message"/></a>
-    <a class="LienImage" href="adminBackOffice.php"><img class="backOffice" src="../Vue/img/backoffice.png" alt="logo backOffice"/></a>
+    <section2>
+        <a class="LienImage" href="analyseBackOffice.php"><img class="analyse" src="../Vue/img/analyse.png" alt="logo analyse"/></a>
+        <p> Analyse des comptes</p>
+    </section2>
+    <section2>
+        <a class="LienImage" href="messagerieBackOffice.php"><img class="messagerie" src="../Vue/img/messagerie.png" alt="logo message"/></a>
+        <p> Messagerie interne</p>
+    </section2>
+    <section2>
+        <a class="LienImage" href="adminBackOffice.php"><img class="backOffice" src="../Vue/img/backoffice.png" alt="logo backOffice"/></a>
+        <p> BackOffice</p>
+    </section2>
 </div>
-
+</div>
 </body>
 <?php include("BasDePage.php"); ?>
 
