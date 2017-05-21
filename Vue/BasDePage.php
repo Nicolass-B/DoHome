c98930b796cecd6b5320242dc4b476f034b5ba47
ali <alionur_93@hotmail.com>
2017-05-21 15:58:49 +0200

    Amélioration de la vue

--- Vue/BasDePage.php
+++ Vue/BasDePage.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html xmlns:justify xmlns:text-justify="http://www.w3.org/1999/xhtml">
 
 <head>
     <link rel="stylesheet" href="../css/HautBasDePage.css"/>
     <meta charset="UTF-8">
-    <title>Nom de la page</title>
+    <title>Bas de page</title>
 </head>
 
 <footer>
@@ -15,3 +14,5 @@
 
 
 </footer>
+
+</html>
