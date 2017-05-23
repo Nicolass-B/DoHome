99788d1f7699a643108356319a220499e07c2180
ali <alionur_93@hotmail.com>
2017-05-23 09:09:10 +0200

    Amélioration de la vue MODIFICATION DES NOMS EN MINUSCULE !!!!! Mise en forme de la messagerie client

--- Vue/messagerie.php
+++ Vue/messagerie.php
@@ -8,11 +8,34 @@
 <?php include("haut_de_page.php"); ?>
 
 <body>
+<section>
+    <fieldset>
 
+        <div class="information">
+            <legend><h2>Contactez-nous</h2></legend>
+            <form method="post" action="">
+                <div class="test">
+                    <ul>
+                        <li>
+                            <label>Objet du message</label> </br>
+                            <input type="text" name="objet" placeholder="Entrez l'objet de votre message" required/>
+                        </li>
+                        <li>
+                            <label>Message</label> </br>
+                            <textarea type="text" name="message" placeholder="Entrez votre message"></textarea>
+                        </li>
+                    </ul>
 
+                </div>
+            </form>
+            <div class="bouton3">
+                <input type="button" value="Envoyer votre message" href="lien" id="bouton3"/>
+            </div>
+        </div>
 
+    </fieldset>
+</section>
 
-<body>
 
 </body>
 
